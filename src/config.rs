f82cac22b3035cc6dec3105a85be49b817622ce9
91d906h4 <fairymanning@gmail.com>
2024-03-23 17:15:15 +0900

    fix: remove test code in config.rs

--- src/config.rs
+++ src/config.rs
@@ -3,16 +3,12 @@ use crate::datastruct::Config;
 
 use toml;
 use std::fs;
-use std::thread;
-use std::time::Duration;
 
 pub fn read() -> Config {
     // Defualt variables.
     let filename = "src/conf/config.toml";
     let mut content: String = String::new();
     let mut config: Config = Config::new();
-
-    // thread::sleep(Duration::from_millis(1000));
 
     // Open config file and read contents.
     match fs::read_to_string(filename) {
